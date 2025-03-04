90c39a241109f02a0c2c362338b4f9c9d2c1894f
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-04 21:32:21 +0200

    Update MainTeleport.lua

--- MainTeleport.lua
+++ MainTeleport.lua
@@ -1,6 +1,6 @@
 -- Создаем ScreenGui
 local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "SpeedGui"
+screenGui.Name = "TeleportGui"
 screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 -- Создаем главное окно
