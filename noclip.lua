45077871e4d93aa78b1098922f9a76fd75c103b9
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-04 21:37:00 +0200

    Update noclip.lua

--- noclip.lua
+++ noclip.lua
@@ -160,3 +160,7 @@ userInputService.InputBegan:Connect(function(input, gameProcessed)
         end
     end
 end)
+
+-- Меню сохраняется после смерти
+screenGui.ResetOnSpawn = false
+screenGui.Parent = game.CoreGui
