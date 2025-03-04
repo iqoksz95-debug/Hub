0accc956b18fcae284bec7dc0eefd6c5d4c53939
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-04 21:45:55 +0200

    Update noclip.lua

--- noclip.lua
+++ noclip.lua
@@ -146,21 +146,6 @@ stopButton.MouseButton1Click:Connect(function()
     toggleNoclip(false)
 end)
 
--- Бинд на скрытие и показ окна с анимацией
-local userInputService = game:GetService("UserInputService")
-local guiVisible = true
-userInputService.InputBegan:Connect(function(input, gameProcessed)
-    if gameProcessed then return end
-    if input.KeyCode == Enum.KeyCode.J then
-        guiVisible = not guiVisible
-        if guiVisible then
-            mainWindow:TweenPosition(UDim2.new(0.35, 0, 0.3, 0), "Out", "Quad", 0.5, true)
-        else
-            mainWindow:TweenPosition(UDim2.new(0.35, 0, -0.5, 0), "Out", "Quad", 0.5, true)
-        end
-    end
-end)
-
 -- Меню сохраняется после смерти
 screenGui.ResetOnSpawn = false
 screenGui.Parent = game.CoreGui
