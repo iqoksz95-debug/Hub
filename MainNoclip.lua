4bd8b40343cf20f25b59cd42cac11fe5087f1fae
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-15 14:54:07 +0200

    Update MainNoclip.lua

--- MainNoclip.lua
+++ MainNoclip.lua
@@ -6,8 +6,8 @@ screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 -- Создаем главное окно
 local mainWindow = Instance.new("Frame")
 mainWindow.Name = "MainWindow"
-mainWindow.Size = UDim2.new(0, 450, 0, 300)
-mainWindow.Position = UDim2.new(0.35, 0, 0.3, 0)
+mainWindow.Size = UDim2.new(0, 300, 0, 200)
+mainWindow.Position = UDim2.new(0.5, -150, 0.5, -100)
 mainWindow.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 mainWindow.BackgroundTransparency = 0.4
 mainWindow.Active = true
@@ -59,15 +59,19 @@ end)
 
 -- Поле для отображения статуса
 local statusLabel = Instance.new("TextBox")
-statusLabel.Size = UDim2.new(0.8, 0, 0, 50)
+statusLabel.Size = UDim2.new(0.8, 0, 0, 30)
 statusLabel.Position = UDim2.new(0.1, 0, 0.25, 0)
 statusLabel.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 statusLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 statusLabel.PlaceholderText = "Noclip: Выключен"
 statusLabel.Font = Enum.Font.SourceSansBold
-statusLabel.TextSize = 24
+statusLabel.TextSize = 18
 statusLabel.Parent = mainWindow
 statusLabel.TextEditable = false
+
+local closeCorner = Instance.new("UICorner")
+closeCorner.CornerRadius = UDim.new(0, 4)
+closeCorner.Parent = statusLabel
 
 statusLabel.MouseEnter:Connect(function()
     statusLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
@@ -78,14 +82,18 @@ end)
 
 -- Кнопка старта
 local startButton = Instance.new("TextButton")
-startButton.Size = UDim2.new(0.8, 0, 0, 50)
+startButton.Size = UDim2.new(0.8, 0, 0, 30)
 startButton.Position = UDim2.new(0.1, 0, 0.45, 0)
 startButton.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 startButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 startButton.Text = "Включить Noclip"
 startButton.Font = Enum.Font.SourceSansBold
-startButton.TextSize = 20
+startButton.TextSize = 18
 startButton.Parent = mainWindow
+
+local closeCorner = Instance.new("UICorner")
+closeCorner.CornerRadius = UDim.new(0, 4)
+closeCorner.Parent = startButton
 
 startButton.MouseEnter:Connect(function()
     startButton.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
@@ -96,14 +104,18 @@ end)
 
 -- Кнопка остановки
 local stopButton = Instance.new("TextButton")
-stopButton.Size = UDim2.new(0.8, 0, 0, 50)
+stopButton.Size = UDim2.new(0.8, 0, 0, 30)
 stopButton.Position = UDim2.new(0.1, 0, 0.65, 0)
 stopButton.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 stopButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 stopButton.Text = "Выключить Noclip"
 stopButton.Font = Enum.Font.SourceSansBold
-stopButton.TextSize = 20
+stopButton.TextSize = 18
 stopButton.Parent = mainWindow
+
+local closeCorner = Instance.new("UICorner")
+closeCorner.CornerRadius = UDim.new(0, 4)
+closeCorner.Parent = stopButton
 
 stopButton.MouseEnter:Connect(function()
     stopButton.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
